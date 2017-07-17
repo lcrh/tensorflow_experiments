6c81c0112ea5dec646a3b43e5adad1af52b998b1
lcrh <leopoldhaller@gmail.com>
2017-07-17 16:35:35 -0700

    Attempt add tensorflow dependency.

---
+++ examples/learn_xor.py
@@ -0,0 +1,10 @@
+"""Build a binary classifier that learns an XOR using tensorflow."""
+
+import numpy as np
+import tensorflow
+
+def main():
+    print "Hello!"
+
+if __name__ == "__main__":
+    main()
